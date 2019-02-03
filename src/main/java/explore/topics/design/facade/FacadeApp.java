b31852e4eab55cd2506837e9ff9e8890049d1eae
kapoordcu <gaurav.kapoor23@outlook.com>
2019-02-03 14:40:12 -0800

    Facade main class added

---
+++ src/main/java/explore/topics/design/facade/FacadeApp.java
@@ -0,0 +1,17 @@
+package explore.topics.design.facade;
+
+/*
+Provide a unified interface to a set of interfaces in a subsystem. Facade defines a higher-level interface that makes the subsystem easier to use.
+This pattern involves a single class which provides simplified methods required by client and delegates calls
+to methods of existing system classes.
+
+Wrap a complicated subsystem with a simpler interface.
+
+http://csharp-video-tutorials.blogspot.com/2018/02/facade-design-pattern.html
+*/
+
+public class FacadeApp {
+    public static void main(String[] args) {
+
+    }
+}
