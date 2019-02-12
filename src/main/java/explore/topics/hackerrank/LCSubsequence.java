c9a94fda9c9d93eb41b88cea0aab3e2714de0587
kapoorg <gaurav.kapoor@tipico.com>
2019-02-12 17:16:42 +0100

    longest Common SubSequence problem main added

---
+++ src/main/java/explore/topics/hackerrank/LCSubsequence.java
@@ -0,0 +1,12 @@
+package explore.topics.hackerrank;
+
+public class LCSubsequence {
+    public static void main(String[] args) {
+        System.out.println(longestCommonSubSequence("ABCDGH", "AEDFHR"));
+        System.out.println(longestCommonSubSequence("AGGTAB", "GXTXAXB"));
+    }
+
+    private static String longestCommonSubSequence(String first, String second) {
+        return first;
+    }
+}
