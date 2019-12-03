46b12afa47338de64812e044773af6a027a2d3c1
Gaurav Kapoor <Gaurav.kapoor@tipico.com>
2019-12-03 16:49:56 +0100

    JAVA-1649: 4 Question solutions modified

--- src/main/java/explore/topics/hackerrank/FindNumberYesNo.java
+++ src/main/java/explore/topics/hackerrank/FindNumberYesNo.java
@@ -2,17 +2,15 @@ package explore.topics.hackerrank;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class FindNumberYesNo {
+
     static String findNumber(List<Integer> arr, int k) {
-        boolean present = false;
-        for (Integer num:arr) {
-            if(num == k) {
-                present = present || true;
-            }
-        }
-        if(present) { return "YES"; }
-        return "NO";
+        Optional<Integer> present = arr.stream()
+                .filter(number -> number == k)
+                .findFirst();
+        return present.isPresent() ? "YES" : "NO";
     }
 
     public static void main(String[] args) {
@@ -22,6 +20,8 @@ public class FindNumberYesNo {
         numberList.add(6);
         numberList.add(1);
         numberList.add(3);
+
+        System.out.println(findNumber(numberList, 3));
 
     }
 }
