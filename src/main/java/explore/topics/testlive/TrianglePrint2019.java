9263e155cf7e6d7de6281aaa823a733d3651406d
Gaurav Kapoor <Gaurav.kapoor@tipico.com>
2019-12-04 16:43:37 +0100

    JAVA-1643: Triangle pattern printed

--- src/main/java/explore/topics/testlive/TrianglePrint2019.java
+++ src/main/java/explore/topics/testlive/TrianglePrint2019.java
@@ -3,16 +3,38 @@ package explore.topics.testlive;
 public class TrianglePrint2019 {
     public static void main(String[] args) {
         int depth = 4;
-        for (int i = 0; i < depth; i++) {
-            System.out.print(printChar(' ', depth-i+1) );
-            System.out.print(printChar('*', 2*i+1) );
-            System.out.println();
+
+        for (int i = 1; i <= depth; i++) {
+            System.out.println(printLTraingle('*', i));
+        }
+        for (int i = 1; i <= depth; i++) {
+            printRevertLTraingle('*', i, depth);
         }
     }
 
-    public static String printChar(Character c, int times) {
+    public static String printLTraingle(char ch, int depth) {
+//        *
+//        **
+//        ***
+//        ****
+        return printCharNTimes(ch, depth);
+    }
+
+    public static void printRevertLTraingle(char ch, int i, int depth) {
+//              *
+//             **
+//            ***
+//           ****
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < times; i++) {
+        builder.append(printCharNTimes(' ', (depth-i)));
+        builder.append(printCharNTimes(ch, i));
+        System.out.println(builder.toString());
+        builder.setLength(0);
+    }
+
+    public static String printCharNTimes(char c, int n) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
             builder.append(c);
         }
         return builder.toString();
