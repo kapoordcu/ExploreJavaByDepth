064af6b24b0429f113200a759f5cb68957bbb107
Gaurav Kapoor <Gaurav.kapoor@tipico.com>
2019-12-05 10:16:13 +0100

    JAVA-1643: Christmas pattern printed

--- src/main/java/explore/topics/testlive/TrianglePrint2019.java
+++ src/main/java/explore/topics/testlive/TrianglePrint2019.java
@@ -4,12 +4,14 @@ public class TrianglePrint2019 {
     public static void main(String[] args) {
         int depth = 4;
 
-        for (int i = 1; i <= depth; i++) {
-            System.out.println(printLTraingle('*', i));
-        }
-        for (int i = 1; i <= depth; i++) {
-            printRevertLTraingle('*', i, depth);
-        }
+//        for (int i = 1; i <= depth; i++) {
+//            System.out.println(printLTraingle('*', i));
+//        }
+//        for (int i = 1; i <= depth; i++) {
+//            printRevertLTraingle('*', i, depth);
+//        }
+
+        printChristmasTree('*', depth);
     }
 
     public static String printLTraingle(char ch, int depth) {
@@ -30,6 +32,18 @@ public class TrianglePrint2019 {
         builder.append(printCharNTimes(ch, i));
         System.out.println(builder.toString());
         builder.setLength(0);
+    }
+
+//              *
+//             ***
+//            *****
+//           *******
+    public static void printChristmasTree(char ch, int depth) {
+        for (int i = 0; i < depth; i++) {
+            String space = printCharNTimes(' ', (depth - i));
+            String pattern = printCharNTimes(ch, (2*i+1));
+            System.out.println(space+pattern);
+        }
     }
 
     public static String printCharNTimes(char c, int n) {
