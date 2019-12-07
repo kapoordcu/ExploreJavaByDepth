2e08c34425129a9a47632591a1bd066696049bf2
gauravDublin <Irish2017#>
2019-12-07 13:43:56 +0100

    JAVA-1343: Equilibrium array implemented

--- src/main/java/explore/topics/testlive/EquilibriumArray2019.java
+++ src/main/java/explore/topics/testlive/EquilibriumArray2019.java
@@ -1,47 +1,40 @@
 package explore.topics.testlive;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class EquilibriumArray2019 {
     public static void main(String[] args) {
-        int[] arrayEq1 = {4, 1, 1, 1, 1};
-        int[] arrayEq2 = {4, -1, 1, 1, 1};
+        int[] arrayEq1 = {4, 1, 1, 2, 3};
+        int[] arrayEq2 = {4, -1, 1, 2, 1};
         int[] arrayEq3 = {2, 1, 1, 3, 1};
         int[] arrayEq4 = {2, 1, 2, 3, 1};
-        //System.out.println(isBalancedArray(arrayEq1));
-        //System.out.println(isBalancedArray(arrayEq2));
+        System.out.println(isBalancedArray(arrayEq1));
+        System.out.println(isBalancedArray(arrayEq2));
         System.out.println(isBalancedArray(arrayEq3));
-       // System.out.println(isBalancedArray(arrayEq4));
+       System.out.println(isBalancedArray(arrayEq4));
     }
 
-    private static boolean isBalancedArray(int[] arrayEq) {
+    private static int isBalancedArray(int[] arrayEq) {
+        List<Integer> integerList = Arrays.stream(arrayEq).boxed().collect(Collectors.toList());
         for (int i = 0; i < arrayEq.length - 1; i++) {
-            for (int j = i+1; j < arrayEq.length; j++) {
-                int left = sumLeft(arrayEq, i);
-                int right = sumRightPart(arrayEq, j);
-
-                System.out.println(left);
-                System.out.println(right);
-                if(left==right) {
-                    return true;
-                }
+            int left = sumLeft(integerList, i+1);
+            int right = sumRightPart(integerList, i+2);
+            if(left==right) {
+                return i+1;
             }
-            System.out.println("----");
         }
-        return false;
+        return -1;
     }
 
-    private static int sumLeft(int[] arrayEq, int endIndex) {
-        int sum = 0;
-        for (int i = 0; i <=endIndex ; i++) {
-            sum+= arrayEq[i];
-        }
-        return sum;
+    private static int sumLeft(List<Integer> integerList, int endIndex) {
+        return integerList.subList(0, endIndex)
+                .stream().mapToInt(Integer::intValue).sum();
     }
 
-    private static int sumRightPart(int[] arrayEq, int startIndex) {
-        int sum = 0;
-        for (int i = startIndex; i < arrayEq.length; i++) {
-            sum+= arrayEq[i];
-        }
-        return sum;
+    private static int sumRightPart(List<Integer> integerList, int startIndex) {
+        return integerList.subList(startIndex, integerList.size())
+                .stream().mapToInt(Integer::intValue).sum();
     }
 }
