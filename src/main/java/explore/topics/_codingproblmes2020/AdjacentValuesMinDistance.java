4615e5c71d1236444a15e1ed4e5eee7fff9e2dcf
gauravDublin <Irish2017#>
2020-01-05 09:18:16 +0100

    JAVA-0918: adjacency matrix

--- src/main/java/explore/topics/_codingproblmes2020/AdjacentValuesMinDistance.java
+++ src/main/java/explore/topics/_codingproblmes2020/AdjacentValuesMinDistance.java
@@ -1,72 +1,52 @@
 package explore.topics._codingproblmes2020;
 
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AdjacentValuesMinDistance {
     public static void main(String[] args) {
-        int[] integers = { 1, 4, 7 , 3, 3, 5 };
-        System.out.println(AdjacentValuesMinDistance.solutionOptimal(integers));
+        int[] A = { 1, 4, 7, 3, 3, 5};
+        AdjacentValuesMinDistance distance = new AdjacentValuesMinDistance();
+        System.out.println(distance.solution(A));
 
     }
-
-    public static int solutionOptimal(int[] A) {
-        List<Pair> elements = new ArrayList<>();
-        for (int i = 0; i < A.length; i++) {
-            elements.add(new Pair(A[i], i));
+    public int solution(int[] A) {
+        List<Integer> elements = new ArrayList<>();
+        Set<Integer> arrToList = IntStream.of(A).boxed().collect(Collectors.toSet());
+        int arrLen = A.length;
+        if(arrLen<1 || arrToList.size()==1) {
+            return -1;
         }
-        Collections.sort(elements, Comparator.comparing(Pair::getValue));
-        Map<Integer, List<Pair>> groupByValues = elements.stream()
-                .collect(Collectors.groupingBy(Pair::getValue));
-        return 0;
-    }
-
-    public static int solution(int[] A) {
-        if(A.length==0) {
-            return 0;
-        }
-        int min = Integer.MAX_VALUE;
-        int arrLen = A.length; // 6
         for (int i = 0; i < arrLen; i++) {
-            for (int j = i+1; j < arrLen; j++) {
+            for (int j = i + 1; j < arrLen; j++) {
                 if(A[i]!=A[j]) {
-                    if(!checkAdjacent(A, Math.min(A[i], A[j]), Math.max(A[i], A[j]))) {
-                        min = Math.abs(i-j) < min ?  Math.abs(i-j): min;
+                    Boolean adjacentPair = checkAdjacency(arrToList, A[i], A[j]);
+                    if(adjacentPair) {
+                        elements.add(Math.abs(i - j));
                     }
                 }
             }
         }
-        return min;
+
+        return Collections.min(elements);
     }
 
-    private static boolean checkAdjacent(int[] A, int i, int j) {
-        List<Integer> elements = IntStream.of(A).boxed().collect(Collectors.toList());
-        return elements.stream()
-                .anyMatch(v -> v > i && v < j);
+    private Boolean checkAdjacency(Set<Integer> A, int a, int b) {
+        int sum = a + b;
+        int min = Math.min(a, b);
+        int max = sum - min;
+        if(max-min>1) {
+            List<Integer> range = IntStream.range(min+1, max).boxed().collect(Collectors.toList());
+            range.retainAll(A);
+            if(range.size()>0) {
+                return false;
+            }
+            return true;
+        }
+
+        return true;
     }
 
-}
-
-class Pair {
-    private Integer value;
-    private Integer index;
-
-    public Pair(Integer value, Integer index) {
-        this.value = value;
-        this.index = index;
-    }
-
-    public Integer getValue() {
-        return value;
-    }
-
-    public Integer getIndex() {
-        return index;
-    }
 }
