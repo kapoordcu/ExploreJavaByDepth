6bbb5f16de2d91d0b0e8736ca3ba62b367a580a7
gauravDublin <Irish2017#>
2020-01-05 06:59:53 +0100

    JAVA-0513: Battleship problem with 75% score solved

--- src/main/java/explore/topics/_codingproblmes2020/Battleship.java
+++ src/main/java/explore/topics/_codingproblmes2020/Battleship.java
@@ -1,125 +1,58 @@
-package explore.topics._codingproblmes2020;
+package explore.topics._codingproblmes2020;// you can also use imports, for example:
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class Battleship {
-
-    public static void main(String[] args) {
-        String S = "1A 1B,2C 2C";
-        String T = "1B";
-
-        Battleship ground = new Battleship();
-        System.out.println(ground.solution(10, S, T));
-    }
-    private List<Point> positionHitPoints(int n, String hitPoints) {
-        List<Point> hitsList = new ArrayList<Point>(n*n);
-
-        String[] coords = hitPoints.split(" ");
-        for (String coord : coords) {
-            hitsList.add(new Point(coord));
-        }
-        return hitsList;
-
-    }
-
-    public List<Ship> positionShips(int n, String ships) {
-        List<Ship> shipList = new ArrayList<Ship>(n*n);
-        String[] shipsCoords = ships.split(",");
-        for (String shipCoord : shipsCoords) {
-            String[] coords = shipCoord.split(" ");
-            shipList.add(new Ship(new Point(coords[0]), new Point(coords[1])));
-        }
-        return shipList;
-    }
+class Battleship {
+    private static String COMME_DELIMITER = ",";
+    private static String SPACE_DELIMITER = " ";
+    private static String COLUN_DELIMITER = ":";
 
     public String solution(int N, String S, String T) {
+        AtomicInteger hits = new AtomicInteger();
+        AtomicInteger sunk = new AtomicInteger();
+        List<String> pairs = Arrays.asList(S.split(COMME_DELIMITER));
+        List<String> xxx = Arrays.asList(T.split(SPACE_DELIMITER));
+        for (String pair: pairs) {
+            calculateBattle(pair, xxx, hits, sunk);
+        }
+        return sunk + "," + hits;
+    }
 
-        List<Ship> ships = positionShips(N, S);
-        List<Point> hits = positionHitPoints(N, T);
-
-        int hit = 0;
-        int sunk = 0;
-
-        Iterator<Ship> shipIterator = ships.iterator();
-        while (shipIterator.hasNext()) {
-            Ship current = shipIterator.next();
-            int touching = (int)current.countHits(hits);
-            if (touching > 0) {
-                if (touching == current.getSize()) {
-                    sunk++;
-                } else {
-                    hit++;
-                }
+    private void calculateBattle(String pair, List<String> xxx, AtomicInteger hits, AtomicInteger sunk) {
+        List<String> shipAreaList = findAllBoxes(pair);
+        List<String> common = new ArrayList<>(shipAreaList);
+        common.removeAll(xxx);
+        if(common.size()==0) {
+            sunk.incrementAndGet();
+        } else {
+            if(common.size()<shipAreaList.size()) {
+                hits.incrementAndGet();
             }
         }
 
-        return "" + sunk + "," + hit;
     }
 
-}
+    private List<String> findAllBoxes(String pair) {
+        List<String> shipArea = new ArrayList<>();
+        String[] bit = pair.split(SPACE_DELIMITER);
+        String[] low = extract(bit[0]).split(COLUN_DELIMITER);
+        String[] high = extract(bit[1]).split(COLUN_DELIMITER);
 
-class Point {
-    int row;
-    int column;
-
-    public Point(String cellIdentifier) {
-        column = (cellIdentifier.toUpperCase().charAt(1)) - ('A');
-        row = (cellIdentifier.charAt(0)) - ('1');
+        for (int i = Integer.parseInt(low[0]); i <= Integer.parseInt(high[0]); i++) {
+            for(char y = low[1].charAt(0); y <= high[1].charAt(0); y++){
+                shipArea.add(i + "" + y);
+            }
+        }
+        return shipArea;
     }
 
-    public int getRow() {
-        return row;
-    }
-
-    public int getColumn() {
-        return column;
-    }
-
-    public boolean insideShip(Point other) {
-        return row >= other.row && column >= other.column;
-    }
-
-    @Override
-    public String toString() {
-        return "Point{" +
-                "row=" + row +
-                ", column=" + column +
-                '}';
-    }
-}
-class Ship  {
-    private Point topLeft;
-    private Point bottomRight;
-
-    public Ship(Point topLeft, Point bottomRight) {
-        this.topLeft = topLeft;
-        this.bottomRight = bottomRight;
-    }
-
-    public Point getTopLeft() {
-        return topLeft;
-    }
-
-    public Point getBottomRight() {
-        return bottomRight;
-    }
-
-    public int getSize() {
-        return (Math.abs(topLeft.getRow() - bottomRight.getRow()) + 1)
-                * (Math.abs(topLeft.getColumn() - bottomRight.getColumn()) + 1);
-    }
-
-    @Override
-    public String toString() {
-        return "(" + topLeft + ", " + bottomRight + ")";
-    }
-
-    public long countHits(List<Point> shots) {
-        return shots.stream()
-                .filter(point-> point.insideShip(topLeft) && bottomRight.insideShip(point))
-                .count();
-
+    public String extract(String str) {
+        String copy = str;
+        String intPart = str.replaceAll("([a-zA-Z])", "");
+        String charPart = copy.replaceAll("\\d", "");
+        return intPart + COLUN_DELIMITER + charPart;
     }
 }
