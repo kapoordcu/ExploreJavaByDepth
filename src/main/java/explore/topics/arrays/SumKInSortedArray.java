dac1be197472a47cd504204409fe9fe552800d38
Gaurav Kapoor <Gaurav.kapoor@tipico.com>
2020-01-27 10:35:40 +0100

    JAVA-1023: Sum K In Unsorted Array

--- src/main/java/explore/topics/arrays/SumKInSortedArray.java
+++ src/main/java/explore/topics/arrays/SumKInSortedArray.java
@@ -3,15 +3,30 @@ package explore.topics.arrays;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 public class SumKInSortedArray {
     public static void main(String[] args) {
         int[] array = { 0, 1,3, 5, 6,8,9,11,14 };
+        //shuffle(array);
         SumKInSortedArray sore = new SumKInSortedArray();
         sore.findIndicesUsingLowHighPointers(array, 14);
         sore.findIndicesUsingHash(array, 14);
         System.out.println();
+    }
+
+    private static void shuffle(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int random = i + new Random().nextInt(array.length-i);
+            swap(array, i, random);
+        }
+    }
+
+    private static void swap(int[] array, int i, int random) {
+        int temp = array[i];
+        array[i] = array[random];
+        array[random] = temp;
     }
 
     private List<String> findIndicesUsingLowHighPointers(int[] array, int target) {
@@ -32,6 +47,10 @@ public class SumKInSortedArray {
         return indices;
     }
 
+     /*
+        Time Complexity: O(n)
+        Auxiliary Space: O(n) where n is size of array.
+     */
     private int findIndicesUsingHash(int[] array, int target) {
         Set<Integer> sumMap = new HashSet<>();
         int numberPairs = 0;
