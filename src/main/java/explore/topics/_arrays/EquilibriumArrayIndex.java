45b8aaec9f60eacb55970eb957c22b684d72c3f5
gauravDublin <Irish2017#>
2020-01-30 20:44:17 +0100

    JAVA-2044: Equilibrium Array Index

--- src/main/java/explore/topics/_arrays/EquilibriumArrayIndex.java
+++ src/main/java/explore/topics/_arrays/EquilibriumArrayIndex.java
@@ -1,21 +1,43 @@
-package explore.topics.testlive;
+package explore.topics._arrays;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class EquilibriumArray2019 {
+public class EquilibriumArrayIndex {
     public static void main(String[] args) {
+        int[] a = {3, -2, 5, -1, -1, -2, 0, 0 , 1, 3, -2};
         int[] arrayEq1 = {4, 1, 1, 2, 3};
         int[] arrayEq2 = {4, -1, 1, 2, 1};
         int[] arrayEq3 = {2, 1, 1, 3, 1};
         int[] arrayEq4 = {2, 1, 2, 3, 1};
-        System.out.println(findEquilibriumArrayIndex(arrayEq1));
-        System.out.println(findEquilibriumArrayIndex(arrayEq2));
-        System.out.println(findEquilibriumArrayIndex(arrayEq3));
-       System.out.println(findEquilibriumArrayIndex(arrayEq4));
+        System.out.println(findEqIndex(a));
+        System.out.println(findEqIndex(arrayEq1));
+        System.out.println(findEqIndex(arrayEq2));
+        System.out.println(findEqIndex(arrayEq3));
+        System.out.println(findEqIndex(arrayEq4));
     }
 
+    //O(n)
+    private static int findEqIndex(int[] a) {
+        if(a.length<3) {
+            return -1;
+        }
+        int totalSum = IntStream.of(a).sum();
+        int leftSum = 0;
+        int righsum = 0;
+        for (int i = 0; i < a.length; i++) {
+            righsum = totalSum - leftSum - a[i];
+            if(leftSum==righsum) {
+                return i;
+            }
+            leftSum += a[i];
+        }
+        return -1;
+    }
+
+    //O(n^2)
     private static int findEquilibriumArrayIndex(int[] arrayEq) {
         List<Integer> integerList = Arrays.stream(arrayEq).boxed().collect(Collectors.toList());
         for (int i = 0; i < arrayEq.length - 1; i++) {
