20d7d265d0ec20993b8e262d743103e2d68dad05
gauravDublin <Irish2017#>
2020-02-01 20:47:11 +0100

    JAVA-0131: Quick Sort imporoved

--- src/main/java/explore/topics/_ds/specialsort/QuickSort.java
+++ src/main/java/explore/topics/_ds/specialsort/QuickSort.java
@@ -24,6 +24,9 @@ public class QuickSort {
     }
 
     private static void swap(Comparable[] a, int i, int j) {
+        if(i==j) {
+            return;
+        }
         Comparable temp = a[i];
         a[i] = a[j];
         a[j] = temp;
@@ -32,18 +35,21 @@ public class QuickSort {
     public static void main(String[] args) {
         QuickSort sort = new QuickSort();
 
+        Comparable arrSorted[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         Comparable arr[] = {12, 11, 13, 5, 6, 7};
         Comparable[] arr1 = {7, 3, 2, 5, 1, 4, 6};
         Comparable[] arr1Duplicate = {7, 3, 2, 5, 1, 4, 6, 2, 3};
         Comparable[] arr2 = {3, 6, 0, 5, 2, 7, 9};
         Comparable[] arr2Duplicate = {3, 6, 0, 9, 2, 7, 0, 9, 9};
 
+        sort.quickSort(arrSorted, 0, arrSorted.length-1);
         sort.quickSort(arr, 0, arr.length-1);
         sort.quickSort(arr1, 0, arr1.length-1);
         sort.quickSort(arr1Duplicate, 0, arr1Duplicate.length-1);
         sort.quickSort(arr2, 0, arr2.length-1);
         sort.quickSort(arr2Duplicate, 0, arr2Duplicate.length-1);
 
+        System.out.println(Arrays.toString(arrSorted));
         System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(arr1));
         System.out.println(Arrays.toString(arr1Duplicate));
