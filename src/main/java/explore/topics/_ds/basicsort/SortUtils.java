7ab244c6651bcba729b03c34f43b0dbfe6f2a148
Gaurav Kapoor <gaurav.kapoor@tipico.com>
2020-08-06 18:11:08 +0200

    JAVA-1808: Misiing SortUtils added just to avoid compile wrror

---
+++ src/main/java/explore/topics/_ds/basicsort/SortUtils.java
@@ -0,0 +1,17 @@
+package explore.topics._ds.basicsort;
+
+public class SortUtils {
+    public static Comparable[] arr3;
+    public static String numbertimesExchangeCalled;
+    public static String numbertimesLessCalled;
+    public static Comparable[] arr1;
+    public static Comparable[] arr2;
+
+    public static boolean less(Comparable comparable, Comparable comparable1) {
+        return false;
+    }
+
+    public static void exchange(Comparable[] pq, int max, int i) {
+
+    }
+}
