46034f0c7fa226657f7c7e7eedc74f9e9430aa8a
Gaurav Kapoor <gaurav.kapoor@tipico.com>
2020-08-26 15:10:55 +0200

    JAVA-0031: Own HASHMAP implementation

--- data-struct/src/main/java/ds/own/my/HashMapGK.java
+++ data-struct/src/main/java/ds/own/my/HashMapGK.java
@@ -2,91 +2,112 @@ package ds.own.my;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class HashMapGK<K, V> {
     private static final int CAPACITY = 16;
     private int initCapacity;
     private int size;
-    private HMNode<K, V>[] bucket;
+    private Entry<K, V>[] buckets;
 
     public HashMapGK() {
         this.initCapacity = CAPACITY;
-        this.bucket = new HMNode[CAPACITY];
+        this.buckets = new Entry[CAPACITY];
     }
 
-    public void put(K key, V value) {
-        int hashcode = calculateHash(key);
-        int index = hashcode & (CAPACITY-1);
-        HMNode  node = new HMNode(hashcode, key, value);
-        HMNode<K, V> currentBucket = bucket[index];
-        if(currentBucket==null) {
-            bucket[index] = node;
-            size++;
-        } else {
-            while (currentBucket.next!=null) {
-                if(currentBucket.key.equals(key)) {
-                    currentBucket.value = value;
-                    return;
-                }
-                currentBucket = currentBucket.next;
-            }
-            if(currentBucket.key.equals(key)) {
-                currentBucket.value = value;
-                return;
-            } else {
-                currentBucket.next = node;
-                size++;
-            }
-        }
-    }
+    @Test
+    public void testMyMapWithStringInteger() {
+        HashMapGK<String, Integer> myMap = new HashMapGK<>();
+        myMap.put("Mumbai", 420001);
+        myMap.put("Bangalore", 560100);
+        myMap.put("Delhi", 110001);
+        myMap.put("Chandigarh", 340001);
+        myMap.put("Kerala", 990398);
+        myMap.put("Kerala", 990378);
 
-    public V get(K key) {
-        int hashcode = calculateHash(key);
-        int index = hashcode & (CAPACITY-1);
-        HMNode<K, V> currentBucket = bucket[index];
-        while(currentBucket!=null) {
-            if(currentBucket.key.equals(key)) {
-                return currentBucket.value;
-            }
-            currentBucket = currentBucket.next;
-        }
-        return null;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    private int calculateHash(K key) {
-        return key == null ? 0 : Math.abs(key.hashCode());
+        assertNotNull(myMap);
+        assertEquals(5, myMap.size());
+        assertTrue(myMap.get("Mumbai")==420001);
+        assertTrue(myMap.get("Bangalore")==560100);
+        assertTrue(myMap.get("Delhi")==110001);
+        assertTrue(myMap.get("Kerala")==990378);
+        assertNull(myMap.get("djdk"));
     }
 
 
     @Test
-    public void testMyMap() {
+    public void testMyMapWithString() {
         HashMapGK<String, String> myMap = new HashMapGK<>();
         myMap.put("USA", "Washington DC");
         myMap.put("Nepal", "Kathmandu");
         myMap.put("India", "New Delhi");
         myMap.put("Australia", "Sydney");
         myMap.put("Australia", "Sydney1");
+        myMap.put("FB", "Facebook");
+        myMap.put("Ea", "EA Games");
         assertNotNull(myMap);
-        assertEquals(4, myMap.size());
+        assertEquals(6, myMap.size());
         assertEquals("Kathmandu", myMap.get("Nepal"));
         assertEquals("Sydney1", myMap.get("Australia"));
+        assertEquals("Facebook", myMap.get("FB"));
+        assertEquals("EA Games", myMap.get("Ea"));
+        assertEquals(null, myMap.get("EA"));
         assertEquals(null, myMap.get("Australiass"));
+    }
+
+    private V get(K key) {
+        int hash = getHash(key);
+        int bucketIndex = hash & (CAPACITY-1);
+        Entry<K, V> bucket = buckets[bucketIndex];
+        while (bucket!=null) {
+            if(bucket.key.equals(key)) {
+                return bucket.value;
+            }
+            bucket = bucket.next;
+        }
+        return null;
+    }
+
+    private void put(K key, V value) {
+        int hash = getHash(key);
+        int bucketIndex = hash & (CAPACITY-1);
+        Entry<K, V> bucket = buckets[bucketIndex];
+        Entry addNode = new Entry(hash, key, value);
+        if(bucket==null) {
+            buckets[bucketIndex] = addNode;
+            size++;
+            return;
+        }
+        while (bucket.next!=null) {
+            if(bucket.key.equals(key)) {
+                bucket.value = value;
+            }
+            bucket = bucket.next;
+        }
+        if(bucket.key.equals(key)) {
+            bucket.value = value;
+            return;
+        }
+        bucket.next = addNode;
+        size++;
+    }
+
+    private int getHash(K key) {
+        return key==null ? 0 : key.hashCode();
+    }
+
+    private int size() {
+        return size;
     }
 }
 
-class HMNode<K, V> {
+class Entry<K, V> {
     int hashcode;
     final K key;
     V value;
-    HMNode<K, V> next;
+    Entry<K, V> next;
 
-    public HMNode(int hashcode, K key, V value) {
+    public Entry(int hashcode, K key, V value) {
         this.hashcode = hashcode;
         this.key = key;
         this.value = value;
