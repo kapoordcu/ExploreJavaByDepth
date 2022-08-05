321b3e169d7e432a47d8af9d2facd7cc1ef433e2
kapoordcu <gaurav.kapoor23@outlook.com>
2022-08-06 01:43:31 +0200

    work break problem using recursion

--- SpanishCoder/src/main/java/leet/dp/WordBreak.java
+++ SpanishCoder/src/main/java/leet/dp/WordBreak.java
@@ -2,7 +2,9 @@ package leet.dp;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,11 +14,40 @@ import static org.junit.Assert.assertTrue;
 
 public class WordBreak {
     Set<String> dictionary = new HashSet<>();
-    @Test
-    public void Example0() {
-        String s = "abcd";
-        String[] wordDict = {"a","b","c","ab","bc","abc"};
-        assertFalse(wordBreakPossibleRecursion(s, 0, wordDict));
+
+    private boolean wordBreakPossibleTabularDP(String s, String[] wordDict) {
+        dictionary = Stream.of(wordDict).collect(Collectors.toSet());
+        boolean dp[] = new boolean[s.length()+1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length() ; i++) {
+            for (int j = 0; j < i; j++) {
+                String word = s.substring(j, i);
+                if(dp[j] && dictionary.contains(word)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+    private boolean wordBreakPossibleCached(String s, int pos, String[] wordDict, Map<String, Boolean> cache) {
+        dictionary = Stream.of(wordDict).collect(Collectors.toSet());
+        String word = s.substring(pos);
+        if(word.isEmpty()) {
+            return true;
+        }
+        if(cache.containsKey(word)) {
+            return cache.get(word);
+        }
+        for (int i = pos; i < s.length(); i++) {
+            word = s.substring(pos, i+1);
+            if(dictionary.contains(word) &&
+                    wordBreakPossibleRecursion(s, i+1, wordDict)) {
+                cache.put(word, true);
+                return true;
+            }
+        }
+        return false;
     }
 
     private boolean wordBreakPossibleRecursion(String s, int pos, String[] wordDict) {
@@ -36,10 +67,38 @@ public class WordBreak {
     }
 
     @Test
+    public void ExampleABCD() {
+        String s = "abcd";
+        String[] wordDict = {"ab","bc","cd"};
+        assertTrue(wordBreakPossibleRecursion(s, 0, wordDict));
+        assertTrue(wordBreakPossibleCached(s, 0, wordDict, new HashMap<>()));
+        assertTrue(wordBreakPossibleTabularDP(s, wordDict));
+    }
+
+    @Test
+    public void Exampleleetcode() {
+        String s = "leetcode";
+        String[] wordDict = {"l","code","ee","t"};
+        assertTrue(wordBreakPossibleRecursion(s, 0, wordDict));
+        assertTrue(wordBreakPossibleCached(s, 0, wordDict, new HashMap<>()));
+        assertTrue(wordBreakPossibleTabularDP(s, wordDict));
+    }
+    @Test
+    public void Exampleabcd() {
+        String s = "abcd";
+        String[] wordDict = {"a","b","c","ab","bc","abc"};
+        assertFalse(wordBreakPossibleRecursion(s, 0, wordDict));
+        assertFalse(wordBreakPossibleCached(s, 0, wordDict, new HashMap<>()));
+        assertFalse(wordBreakPossibleTabularDP(s, wordDict));
+    }
+
+    @Test
     public void Example1() {
         String s = "leetcode";
         String[] wordDict = {"leet","code"};
         assertTrue(wordBreakPossibleRecursion(s, 0, wordDict));
+        assertTrue(wordBreakPossibleCached(s, 0, wordDict, new HashMap<>()));
+        assertTrue(wordBreakPossibleTabularDP(s, wordDict));
     }
 
     @Test
@@ -47,6 +106,8 @@ public class WordBreak {
         String s = "applepenapple";
         String[] wordDict = {"apple","pen"};
         assertTrue(wordBreakPossibleRecursion(s, 0, wordDict));
+        assertTrue(wordBreakPossibleCached(s, 0, wordDict, new HashMap<>()));
+        assertTrue(wordBreakPossibleTabularDP(s, wordDict));
     }
 
     @Test
@@ -54,6 +115,8 @@ public class WordBreak {
         String s = "catsandog";
         String[] wordDict = {"cats","dog","sand","and","cat"};
         assertFalse(wordBreakPossibleRecursion(s, 0, wordDict));
+        assertFalse(wordBreakPossibleCached(s, 0, wordDict, new HashMap<>()));
+        assertFalse(wordBreakPossibleTabularDP(s, wordDict));
     }
 
 }
