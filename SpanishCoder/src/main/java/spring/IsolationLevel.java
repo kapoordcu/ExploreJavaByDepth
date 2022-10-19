78c72641bae50754e59258a3e5ee553d0053630d
kapoordcu <gaurav.kapoor23@outlook.com>
2022-10-20 00:39:07 +0200

    Notification service design

---
+++ SpanishCoder/src/main/java/spring/IsolationLevel.java
@@ -0,0 +1,7 @@
+package spring;
+
+public class IsolationLevel {
+    public void checkTransactionLevel() {
+
+    }
+}
--- SpanishCoder/src/main/java/strings/BracketBalance.java
+++ SpanishCoder/src/main/java/strings/BracketBalance.java
@@ -2,18 +2,51 @@ package strings;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class BracketBalance {
+    Map<Character, Character> bracketsMap = new HashMap<>();
+
     @Test
     public void test01() {
         String s = "()";
         assertTrue(isBalanced(s));
+    }
+
+    private boolean isBalanced(String str) {
+        if(bracketsMap.isEmpty()) {
+            bracketsMap.put('(', ')');
+            bracketsMap.put('{', '}');
+            bracketsMap.put('[', ']');
+        }
+
+        Stack<Character> queue = new Stack<>();
+        queue.add(str.charAt(0));
+
+        while (!queue.isEmpty()) {
+            for (int i = 1; i < str.length(); i++) {
+                Character prevBracket = null;
+                if(!queue.isEmpty()) {
+                    prevBracket = queue.peek();
+                }
+                Character currentBracket = str.charAt(i);
+                if(bracketsMap.containsKey(prevBracket) &&
+                        currentBracket.equals(bracketsMap.get(prevBracket))) {
+                    queue.pop();
+                } else {
+                    queue.add(currentBracket);
+                }
+            }
+            if(queue.isEmpty()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
     }
 
     @Test
@@ -34,33 +67,35 @@ public class BracketBalance {
         assertFalse(isBalanced(s));
     }
 
-    private boolean isBalanced(String s) {
-        Stack<Character> stack = new Stack<>();
-//        Map<Character, Character> brackets = new HashMap<>();
-        Character pop = null;
-//        brackets.put('{', '}');
-//        brackets.put('[', ']');
-//        brackets.put('(', ')');
-        for (Character ch: s.toCharArray()) {
-            if(!stack.isEmpty()) {
-                pop = stack.peek();
-            }
-            if(pop!=null && isMatchFound(pop, ch)) {
-                stack.pop();
-                pop = null;
-            } else {
-                stack.push(ch);
-            }
-        }
-        return stack.isEmpty();
-    }
 
-    private boolean isMatchFound(char pop, char ch) {
-        if(pop=='{' && ch=='}' ||
-                pop=='[' && ch==']' ||
-                pop=='(' && ch==')' ) {
-            return true;
-        }
-        return false;
-    }
+
+//    private boolean isBalanced(String s) {
+//        Stack<Character> stack = new Stack<>();
+////        Map<Character, Character> brackets = new HashMap<>();
+//        Character pop = null;
+////        brackets.put('{', '}');
+////        brackets.put('[', ']');
+////        brackets.put('(', ')');
+//        for (Character ch: s.toCharArray()) {
+//            if(!stack.isEmpty()) {
+//                pop = stack.peek();
+//            }
+//            if(pop!=null && isMatchFound(pop, ch)) {
+//                stack.pop();
+//                pop = null;
+//            } else {
+//                stack.push(ch);
+//            }
+//        }
+//        return stack.isEmpty();
+//    }
+//
+//    private boolean isMatchFound(char pop, char ch) {
+//        if(pop=='{' && ch=='}' ||
+//                pop=='[' && ch==']' ||
+//                pop=='(' && ch==')' ) {
+//            return true;
+//        }
+//        return false;
+//    }
 }
